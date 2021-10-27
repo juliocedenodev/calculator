4db6252a163354686b0c93b4cf06d27a86470163
Julio <juliocedenodev@gmail.com>
2021-10-26 21:25:47 -0400

    starting

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(a,b)
+{
+    return a + b;
+}
+
+function subtract(a,b)
+{
+    return a - b;
+}
+function multiply(a,b)
+{
+    return a * b;
+}
+function divide(a,b)
+{
+    return a / b;
+}
+
+function operate(a,b,op) {
+    if(op == '+')
+    {
+        return add(a, b);
+    }
+    if(op == '-')
+    {
+        return subtract(a, b);
+    }
+    if(op == '*')
+    {
+        return multiply(a, b);
+    }
+    if(op == '/')
+    {
+        return divide(a, b);
+    }
+
+    
+}
+
+console.log(operate(5,5,'-'))
